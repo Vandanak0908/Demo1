851b252262d13c989ecb2b48eafb0f314a03f613
Vandanak0908 <98521819+Vandanak0908@users.noreply.github.com>
2022-01-27 15:52:03 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,17 @@
+package class_demo;
+
+public class abc {
+public void Display()
+{                            
+String a;
+String b;
+String c;
+String[]k= {"ndkjfwjemf","amsdkmaskd","nvfjdvnkd"};
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		abc a1=new abc();
+		a1.Display();
+	}
+
+}
